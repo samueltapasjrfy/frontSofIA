63a9b0edee8e3622bb2d1ea4a1ab2bb1d259bb40
igorjurify <igor.santos@jurify.com.br>
2025-04-17 15:55:16 -0300

    change: ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,13 @@
-require('dotenv').config();
+require('dotenv').config({
+  path: '.env.local',
+});
 
 module.exports = {
     apps: [
       {
         name: "SOPHIA_FRONT",
         script: "npm",
-        args: `start -- -p ${process.env.PORT || 3000}`,
+        args: `start -- -p ${process.env.PORT || 3000} `,
         env: {
           NODE_ENV: "production",
         },
