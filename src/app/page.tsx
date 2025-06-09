0ec4ebb04b6680b7de00500879e6359142978285
igorjurify <igor.santos@jurify.com.br>
2025-06-09 14:19:21 -0300

    fix: version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
 "use client";
 import { LoginResponse } from "@/api/authApi";
-import { getLocalStorage, LocalStorageKeys } from "@/utils/localStorage";
+import { getLocalStorage, LocalStorageKeys, setLocalStorage } from "@/utils/localStorage";
 import { redirect } from "next/navigation";
 import { useEffect } from "react";
 
 export default function Home() {
   useEffect(() => {
-    const version = getLocalStorage<string>(LocalStorageKeys.VERSION);
+    let version = getLocalStorage<string>(LocalStorageKeys.VERSION);
+    if (!version || typeof version !== 'string') {
+      version = '1';
+      setLocalStorage(LocalStorageKeys.VERSION, version);
+    }
     const user = getLocalStorage<LoginResponse>(LocalStorageKeys.USER);
+    console.log({ version, user })
     if (user) {
       return redirect(`/v${version}/dashboard`);
     }
